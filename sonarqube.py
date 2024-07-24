0a0eac11c09bbf1d2a4c4a1a518f3d6b55489484
Mykhailo Nikiforov <mn@palkoi.net>
2024-07-24 14:50:48 +0300

    feat: use new generic issue format

--- sonarqube.py
+++ sonarqube.py
@@ -6,11 +6,11 @@ import sys
 
 LOG_PREFIX = "[trivy][plugins][sonarqube]"
 TRIVY_SONARQUBE_SEVERITY = {
-    "UNKNOWN": "INFO",
-    "LOW": "MINOR",
-    "MEDIUM": "MAJOR",
-    "HIGH": "CRITICAL",
-    "CRITICAL": "BLOCKER",
+    "UNKNOWN": "LOW",
+    "LOW": "LOW",
+    "MEDIUM": "LOW",
+    "HIGH": "MEDIUM",
+    "CRITICAL": "HIGH",
 }
 
 
@@ -24,7 +24,7 @@ def parse_trivy_report(report):
         for vuln in result.get("Vulnerabilities", []):
             try:
                 vuln["Target"] = result["Target"]
-                for key in ("VulnerabilityID", "Severity", "Description"):
+                for key in ("VulnerabilityID", "Title", "Description", "Severity", "PrimaryURL"):
                     vuln[key]
             except KeyError:
                 continue
@@ -33,40 +33,52 @@ def parse_trivy_report(report):
 
 
 def make_sonar_issues(vulnerabilities, file_path=None):
-    return [
-        {
-            "engineId": "Trivy",
+    seen_rules = set()
+    res = {"rules": [], "issues": []}
+    for vuln in vulnerabilities:
+        if vuln["VulnerabilityID"] not in seen_rules:
+            res["rules"].append({
+              "id": vuln["VulnerabilityID"],
+              "name": vuln["Title"],
+              "description": vuln["Description"],
+              "engineId": "Trivy",
+              "cleanCodeAttribute": "LOGICAL",
+              "impacts": [{
+                  "softwareQuality": "SECURITY",
+                  "severity": TRIVY_SONARQUBE_SEVERITY[vuln["Severity"]]
+              }]
+            })
+            seen_rules.add(vuln["VulnerabilityID"])
+        res["issues"].append({
             "ruleId": vuln["VulnerabilityID"],
-            "type": "VULNERABILITY",
-            "severity": TRIVY_SONARQUBE_SEVERITY[vuln["Severity"]],
             "primaryLocation": {
-                "message": vuln["Description"],
+                "message": f"{vuln["Description"]} Details: {vuln["PrimaryURL"]}",
                 "filePath": file_path or vuln["Target"],
-            },
-        }
-        for vuln in vulnerabilities
-    ]
+            }
+        })
+    return res
 
 
-def make_sonar_report(issues):
-    return json.dumps({"issues": issues}, indent=2)
+def make_sonar_report(res):
+    return json.dumps(res, indent=2)
 
 
 def main(args):
     fname = args[1]
     if not os.path.exists(fname):
         sys.exit(f"{LOG_PREFIX} file not found: {fname}")
-
+    
     arg_filePath = None
     for arg in args[2:]:
         if "filePath" in arg:
             arg_filePath = arg.split("=")[-1].strip()
-
+    
     report = load_trivy_report(fname)
     vulnerabilities = parse_trivy_report(report)
-    issues = make_sonar_issues(vulnerabilities, file_path=arg_filePath)
-    report = make_sonar_report(issues)
+    res = make_sonar_issues(vulnerabilities, file_path=arg_filePath)
+    report = make_sonar_report(res)
     print(report)
+
 
 
 if __name__ == "__main__":
